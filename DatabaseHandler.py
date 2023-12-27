26c0b7bdfd9d92eb3f410d396429e16074d91104
Christoph Hehl <christophhehl363@gmail.com>
2023-12-27 12:09:25 +0100

    final version for now

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -11,6 +11,22 @@ def get_adresses(cursor):
     return json.dumps(formatted)
 
 
+def get_searched_adresses(cursor, path):
+    path = path[1:]
+    search_texts = path.split("%20")
+    formatted = {}
+    for search in search_texts:
+        search_value = "%" + search + "%"
+        cursor.execute(
+            "SELECT * FROM adressen "
+            "WHERE Nachname like ? OR Vorname LIKE ? OR Ehepartner LIKE ? OR Straße LIKE ? OR Hausnummer LIKE ? OR "
+            "Postleitzahl LIKE ? OR Ort LIKE ? OR Kinder LIKE ?", [search_value] * 8)
+        unformatted = cursor.fetchall()
+        for adress in unformatted:
+            formatted[adress[0]] = adress[1:]
+    return json.dumps(formatted)
+
+
 def create_adressen_table(cursor):
     cursor.execute("""
                     CREATE TABLE adressen
@@ -22,7 +38,8 @@ def create_adressen_table(cursor):
                         Straße       varchar(255),
                         Hausnummer   varchar(255),
                         Postleitzahl varchar(255),
-                        Ort          varchar(255)
+                        Ort          varchar(255),
+                        Kinder       varchar(255)
                     )
     """)
 
@@ -39,4 +56,29 @@ def create_tables(connection):
 def connect_or_create():
     connection = sqlite3.connect("adressbuch.db")
     create_tables(connection)
-    return connection.cursor()
+    return connection
+
+
+def add_contact(connection, data):
+    cursor = connection.cursor()
+    print(data)
+    if data[0] == '-1':
+        cursor.execute(
+            "INSERT INTO adressen (nachname, vorname, ehepartner, straße, hausnummer, postleitzahl, ort, kinder)"
+            " values (?,?,?,?,?,?,?,?)",
+            data[1:])
+    else:
+        reordered_data = data[1:]
+        reordered_data.append(data[0])
+        print(reordered_data)
+        cursor.execute(
+            "UPDATE adressen SET Nachname=?,Vorname=?,Ehepartner=?,Straße=?,Hausnummer=?,Postleitzahl=?,Ort=?,Kinder=? "
+            "WHERE id = ?", reordered_data)
+    connection.commit()
+
+
+def delete_contact(connection, id_string):
+    cursor = connection.cursor()
+    entry_id = int(id_string)
+    cursor.execute("DELETE FROM adressen WHERE id = ?", [entry_id])
+    connection.commit()
--- adressbuch-server.py
+++ adressbuch-server.py
@@ -1,17 +1,43 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import DatabaseHandler
 
 hostName = "localhost"
 serverPort = 2323
-database_cur = DatabaseHandler.connect_or_create()
+database_con = DatabaseHandler.connect_or_create()
+database_cur = database_con.cursor()
 
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
-        self.wfile.write(bytes(DatabaseHandler.get_adresses(database_cur), "utf-8"))
+        if self.path == "/":
+            self.wfile.write(bytes(DatabaseHandler.get_adresses(database_cur), "utf-8"))
+        else:
+            self.wfile.write(bytes(DatabaseHandler.get_searched_adresses(database_cur, self.path), "utf-8"))
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', "*")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+
+    def do_POST(self):
+        if self.path == "/post":
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            data = json.loads(data_string.decode("utf-8"))
+            DatabaseHandler.add_contact(database_con, data)
+        elif self.path == "/delete":
+            data_string = self.rfile.read(int(self.headers['Content-Length']))
+            print(data_string)
+            DatabaseHandler.delete_contact(database_con, data_string.decode("utf-8"))
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
 
 
 if __name__ == '__main__':
